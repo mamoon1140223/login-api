6be87ab25b8b29d57747bed02afab8a2c62ebabd
mamoon1140223 <mamoon1140223@gmail.com>
2025-08-03 17:05:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // ✅ 資料庫連線設定
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
